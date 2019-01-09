b9c9d2521de69ffa57b887ac2db6bdf26a304a5c
Charles Clemens <cclemensjr@gmail.com>
2019-01-09 10:59:37 -0800

    populated solution array via iterator

--- Challenges/shiftArray/shiftArray/Program.cs
+++ Challenges/shiftArray/shiftArray/Program.cs
@@ -16,22 +16,23 @@ namespace shiftArray
             int[] array = { 2, 6, 4, 8 };
             int toBeAdded = 5;
 
-            InsertShiftArray(array, toBeAdded).ToString();
+            InsertShiftArray(array, toBeAdded);
         }
 
-        static int[] InsertShiftArray(int[] array, int toBeAdded)
+        static int[] InsertShiftArray(int[] inputArray, int toBeAdded)
         {
-            int[] solutionArray = new int[array.Length + 1];
+            int[] solutionArray = new int[inputArray.Length + 1];
             int middleIndex = solutionArray.Length / 2;
 
             for (int i = 0; i < solutionArray.Length; i++)
             {
+                if (i < middleIndex) { solutionArray[i] = inputArray[i]; }
+                if (i > middleIndex) { solutionArray[i] = inputArray[i - 1]; }
 
+                solutionArray[i] = toBeAdded;
             }
 
             return solutionArray;
         }
-
-
     }
 }
