0a5d27adfd26de7347c6e9de36526a1e7855662d
CClemensJr <cclemensjr@gmail.com>
2019-01-08 22:20:11 -0800

    Pushing basic file structure to master branch

--- Challenges/shiftArray/shiftArray/Program.cs
+++ Challenges/shiftArray/shiftArray/Program.cs
@@ -6,7 +6,7 @@ namespace shiftArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
