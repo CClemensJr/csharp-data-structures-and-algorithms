0c91ea1ed776080dd7fdde2faf8a45e168381012
Charles Clemens <cclemensjr@gmail.com>
2019-01-10 08:11:29 -0800

    Created shape of BinarySearch method and called it in main

--- Challenges/binarySearchArray/binarySearchArray/Program.cs
+++ Challenges/binarySearchArray/binarySearchArray/Program.cs
@@ -6,7 +6,17 @@ namespace binarySearchArray
     {
         static void Main(string[] args)
         {
+            int[] arrayToBeSearched = { 4, 8, 15, 16, 23, 42 };
+            int searchKey = 2;
+
+            BinarySearch(arrayToBeSearched, searchKey);
+        }
+
+        public static int BinarySearch(int[] array, int searchKey)
+        {
             Console.WriteLine("Hello World!");
+
+            return -1;
         }
     }
 }
