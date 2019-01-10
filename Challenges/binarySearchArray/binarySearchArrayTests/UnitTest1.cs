8022c538555a1a38b1ec540d6c1702e8e5620c78
Charles Clemens <cclemensjr@gmail.com>
2019-01-10 07:51:22 -0800

    Added test solution to binarySearchArray projecr

---
+++ Challenges/binarySearchArray/binarySearchArrayTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using binarySearchArray;
+
+namespace binarySearchArrayTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
