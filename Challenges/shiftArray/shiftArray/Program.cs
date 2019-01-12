d0519a51249d79e12e5b00710fa9cae83cce09a7
CClemensJr <cclemensjr@gmail.com>
2019-01-12 13:10:30 -0800

    Added comments

--- Challenges/shiftArray/shiftArray/Program.cs
+++ Challenges/shiftArray/shiftArray/Program.cs
@@ -11,6 +11,9 @@ namespace shiftArray
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// The ArrayShift method creates a default array and value to be added to the array. It then sends them to InsertShiftArray and assigns the returned array to a variable that is used to display the resulting array in the console.
+        /// </summary>
         static void ArrayShift()
         {
             int[] array = { 2, 6, 4, 8 };
@@ -26,6 +29,12 @@ namespace shiftArray
             Console.Write("]");
         }
 
+        /// <summary>
+        /// InsertShitArray takes an inputArray of integers and an integer toBeAdded to the array. It then creates a new solutionArray that is one element larger that the inputArray. The method then finds the midpoint of the new array and then iterates over it. During iteration the values in the first half of the inputArray are assigned to the solutionArray. When the middle element of the solutionArray is reached, the integer toBeAdded is assigned to the element and then the remaining half of the inputArray fills up the rest of the solutionArray. The solutionArray is then returned to ArrayShift.
+        /// </summary>
+        /// <param name="inputArray"></param>
+        /// <param name="toBeAdded"></param>
+        /// <returns>An array of integers containing the number to be added</returns>
         static int[] InsertShiftArray(int[] inputArray, int toBeAdded)
         {
             int[] solutionArray = new int[inputArray.Length + 1];
