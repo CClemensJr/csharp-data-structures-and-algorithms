2528c5b681da6aa3c0617609f3477a50f0e97a82
Charles Clemens <cclemensjr@gmail.com>
2019-01-14 08:38:17 -0800

    Created node and llist objects

--- Data-Structures/Linked Lists/LinkedList/LinkedList.cs
+++ Data-Structures/Linked Lists/LinkedList/LinkedList.cs
@@ -7,7 +7,14 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            LList linkedList = new LList()
+            Node node = new Node();
+
+            LList linkedList = new LList(node);
+
+            Console.WriteLine($"LLHEAD = { linkedList.Head.value }");
+
+            Console.Write("\n\nPress any key to continue...");
+            Console.ReadLine();
         }
     }
 }
