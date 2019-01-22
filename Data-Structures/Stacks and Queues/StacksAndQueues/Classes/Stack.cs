71dd5d96e40e1c11a1ef87b50239c0eb4351175f
CClemensJr <cclemensjr@gmail.com>
2019-01-21 21:46:26 -0800

    Added comments to stack class

--- Data-Structures/Stacks and Queues/StacksAndQueues/Classes/Stack.cs
+++ Data-Structures/Stacks and Queues/StacksAndQueues/Classes/Stack.cs
@@ -9,12 +9,21 @@ namespace StacksAndQueues.Classes
         public Node Top { get; set; }
         public Node Temp { get; set; }
 
+        /// <summary>
+        /// This method takes a new node and pushes it into the top of the stack by setting it = to the Top node.
+        /// </summary>
+        /// <param name="node"></param>
         public void Push(Node node)
         {
             node.Next = Top;
             Top = node;
         }
 
+        /// <summary>
+        /// This method removes the node at the Top of the stack and returns it for consumption.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns>The node that was removed from the stack</returns>
         public Node Pop(Node node)
         {
             Temp = Top;
@@ -24,6 +33,10 @@ namespace StacksAndQueues.Classes
             return Temp;
         }
 
+        /// <summary>
+        /// This method returns the node at the Top of the stack.
+        /// </summary>
+        /// <returns>The node that is at the top of the stack</returns>
         public Node Peek()
         {
             return Top;
