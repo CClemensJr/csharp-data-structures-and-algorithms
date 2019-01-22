bfd1071b0ea6f2eee2ec6431fb7800106df8dff5
CClemensJr <cclemensjr@gmail.com>
2019-01-21 21:35:54 -0800

    Added comments to Node class

--- Data-Structures/Stacks and Queues/StacksAndQueues/Classes/Node.cs
+++ Data-Structures/Stacks and Queues/StacksAndQueues/Classes/Node.cs
@@ -9,8 +9,15 @@ namespace StacksAndQueues.Classes
         public int Value { get; set; }
         public Node Next { get; set; }
 
+        /// <summary>
+        /// This constructor allows Node objects with no properties defined to be instantiated.
+        /// </summary>
         public Node() { }
 
+        /// <summary>
+        /// This custom constructor allows a Node object to be created with a set Value property
+        /// </summary>
+        /// <param name="value"></param>
         public Node(int value)
         {
             Value = value;
