94fa36e70b64b8ad9d0feea8b218eddc14b872b4
Charles Clemens <cclemensjr@gmail.com>
2019-01-22 14:32:45 -0800

    Created method signature for Enqueue and Dequeue

---
+++ Challenges/queueWithStacks/queueWithStacks/Classes/PseudoQueue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StacksAndQueues.Classes;
+
+namespace queueWithStacks.Classes
+{
+    public class PseudoQueue
+    {
+        public Stack queueStack { get; set; }
+
+        public void Enqueue(Node node)
+        {
+
+        }
+
+        public Node Dequeue()
+        {
+            return null;
+        }
+    }
+}
