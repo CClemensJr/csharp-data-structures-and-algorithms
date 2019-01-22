80925eae44f3de47a628d754ee6f2d5ae44c82c2
Charles Clemens <cclemensjr@gmail.com>
2019-01-22 09:59:35 -0800

    Completed queue tests

--- Data-Structures/Stacks and Queues/StacksAndQueuesTests/UnitTest1.cs
+++ Data-Structures/Stacks and Queues/StacksAndQueuesTests/UnitTest1.cs
@@ -38,44 +38,85 @@ namespace StacksAndQueuesTests
             Assert.Equal(7, queue.Rear.Value);
         }
 
+        [Fact]
+        public void CanSetFrontNodeIfQueueEmpty()
+        {
+            Queue queue = new Queue();
+            Node node = new Node(7);
+
+            queue.Enqueue(node);
+
+            Assert.Equal(7, queue.Front.Value);
+        }
+
 
         //dequeue
         [Fact]
         public void CanRemoveFromQueue()
         {
+            Queue queue = new Queue();
 
+            Node node1 = new Node(7);
+            Node node2 = new Node(10);
+            Node node3 = new Node(71);
+
+            queue.Enqueue(node1);
+            queue.Enqueue(node2);
+            queue.Enqueue(node3);
+
+            Node removedNode = queue.Dequeue();
+
+            Assert.Equal(7, removedNode.Value);
         }
 
         [Fact]
-        public void CanSetNewFront()
+        public void CanSetFront()
         {
+            Queue queue = new Queue();
 
+            Node node1 = new Node(7);
+            Node node2 = new Node(10);
+            Node node3 = new Node(71);
+
+            queue.Enqueue(node1);
+            queue.Enqueue(node2);
+            queue.Enqueue(node3);
+
+            Assert.Equal(7, queue.Front.Value);
         }
 
         [Fact]
-        public void CanReturnNode()
+        public void CanReturnRemovedNode()
         {
+            Queue queue = new Queue();
 
+            Node node1 = new Node(7);
+            Node node2 = new Node(10);
+            Node node3 = new Node(71);
+
+            queue.Enqueue(node1);
+
+            Node removedNode = queue.Dequeue();
+
+            Assert.Equal(7, removedNode.Value);
         }
 
 
         // peek
         [Fact]
-        public void CanDetectFrontNode()
-        {
-
-        }
-
-        [Fact]
-        public void CanReturnANode()
-        {
-
-        }
-
-        [Fact]
         public void CanReturnTheFrontNode()
         {
+            Queue queue = new Queue();
 
+            Node node1 = new Node(7);
+            Node node2 = new Node(10);
+            Node node3 = new Node(71);
+
+            queue.Enqueue(node1);
+
+            Node nodeInFront = queue.Peek();
+
+            Assert.Equal(7, nodeInFront.Value);
         }
     }
 }
