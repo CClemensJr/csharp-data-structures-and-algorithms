1f3a5bf0a6cfa8e0472850a2433c4af4ccaadf03
Charles Clemens <cclemensjr@gmail.com>
2019-01-21 20:17:36 -0800

    Updated Queue class

--- Data-Structures/Stacks and Queues/StacksAndQueues/Classes/Queue.cs
+++ Data-Structures/Stacks and Queues/StacksAndQueues/Classes/Queue.cs
@@ -6,5 +6,23 @@ namespace StacksAndQueues.Classes
 {
     class Queue
     {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+        public Node Temp { get; set; }
+
+        public void Enqueue()
+        {
+
+        }
+
+        public void Dequeue()
+        {
+
+        }
+
+        public void Peek()
+        {
+
+        }
     }
 }
