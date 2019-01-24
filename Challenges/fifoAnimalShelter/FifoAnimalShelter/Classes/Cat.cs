1540655cabc01eee29b32e0b5c32f98432be3786
Charles Clemens <cclemensjr@gmail.com>
2019-01-24 08:40:00 -0800

    Created cat and dog classes

---
+++ Challenges/fifoAnimalShelter/FifoAnimalShelter/Classes/Cat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FifoAnimalShelter.Classes
+{
+    class Cat
+    {
+        public string Value { get; set; } = "cat";
+    }
+}
---
+++ Challenges/fifoAnimalShelter/FifoAnimalShelter/Classes/Dog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FifoAnimalShelter.Classes
+{
+    class Dog
+    {
+        public string Value { get; set; } = "dog";
+    }
+}
