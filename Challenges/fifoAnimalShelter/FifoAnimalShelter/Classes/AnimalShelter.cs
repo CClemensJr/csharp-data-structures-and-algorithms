23c4a5ec28fa39d72f46dffea36a70ffb952e797
Charles Clemens <cclemensjr@gmail.com>
2019-01-24 08:46:29 -0800

    Created custom constructor for cat dog and queue objects

--- Challenges/fifoAnimalShelter/FifoAnimalShelter/Classes/AnimalShelter.cs
+++ Challenges/fifoAnimalShelter/FifoAnimalShelter/Classes/AnimalShelter.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using StacksAndQueues.Classes;
 
 namespace FifoAnimalShelter.Classes
 {
     class AnimalShelter
     {
-        public Dog dog { get; set; }
-        public Cat cat { get; set; }
-
+        public Dog Dog { get; set; }
+        public Cat Cat { get; set; }
+        public Queue Shelter { get; set; }
         
+        public AnimalShelter()
+        {
+            Dog = new Dog();
+            Cat = new Cat();
+            Shelter = new Queue();
+        }
+
+
     }
 }
