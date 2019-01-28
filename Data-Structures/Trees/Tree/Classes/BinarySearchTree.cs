9e90b36293956379851ad43b54af00d31f982d78
Charles Clemens <cclemensjr@gmail.com>
2019-01-28 08:34:16 -0800

    Add method signature

--- Data-Structures/Trees/Tree/Classes/BinarySearchTree.cs
+++ Data-Structures/Trees/Tree/Classes/BinarySearchTree.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Tree.Classes
 {
-    class BinarySearchTree
+    public class BinarySearchTree
     {
+        public void Add(int value)
+        {
+            // Add a node to
+        }
+
+        public bool Contains(int value)
+        {
+            // Determine if value is in try at least once
+            return false;
+        }
     }
 }
