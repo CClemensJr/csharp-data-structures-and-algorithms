62bcade9982ab9974b2eb1372d9fe3c9fc9a977d
Charles Clemens <cclemensjr@gmail.com>
2019-01-31 07:52:33 -0800

    Created 3 test classes representing each class

--- Data-Structures/Trees/TreeTests/UnitTest1.cs
+++ Data-Structures/Trees/TreeTests/UnitTest1.cs
@@ -3,7 +3,25 @@ using Xunit;
 
 namespace TreeTests
 {
-    public class UnitTest1
+    public class TreeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+
+    public class BinaryTreeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+
+    public class BinarySearchTreeTests
     {
         [Fact]
         public void Test1()
