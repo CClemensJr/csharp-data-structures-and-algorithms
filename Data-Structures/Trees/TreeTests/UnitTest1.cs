89d887567d1ad4f37a0bb8b4d2b91599dab9c15e
Charles Clemens <cclemensjr@gmail.com>
2019-01-31 08:03:35 -0800

    Created passing node tests

--- Data-Structures/Trees/TreeTests/UnitTest1.cs
+++ Data-Structures/Trees/TreeTests/UnitTest1.cs
@@ -1,8 +1,47 @@
 using System;
 using Xunit;
+using Tree.Classes;
 
 namespace TreeTests
 {
+    public class NodeTests
+    {
+        [Fact]
+        public void CanCreateNode()
+        {
+            Node testNode = new Node();
+
+            testNode.Value = 7;
+
+            Assert.True(testNode.Value == 7);
+        }
+
+        [Fact]
+        public void CanGetNodeValue()
+        {
+            Node testNode = new Node();
+
+            testNode.Value = 7;
+
+            int testValue = testNode.Value;
+
+            Assert.True(testValue == testNode.Value);
+        }
+
+        [Fact]
+        public void CanSetNodeValue()
+        {
+            Node testNode = new Node();
+
+            testNode.Value = 7;
+            int testValue = testNode.Value;
+
+            testNode.Value = 89;
+
+            Assert.True(testNode.Value != testValue);
+        }
+    }
+
     public class TreeTests
     {
         [Fact]
