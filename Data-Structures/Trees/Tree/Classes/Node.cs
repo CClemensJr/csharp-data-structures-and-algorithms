29cf6a822f712bf7f121a253ae18eb7de5d7c3ff
CClemensJr <cclemensjr@gmail.com>
2019-02-10 17:14:40 -0800

    Changed node values to int

--- Data-Structures/Trees/Tree/Classes/Node.cs
+++ Data-Structures/Trees/Tree/Classes/Node.cs
@@ -6,12 +6,13 @@ namespace Tree.Classes
 {
     public class Node
     {
-        public object Value { get; set; }
+        public int Value { get; set; }
         public Node LeftChild { get; set; }
         public Node RightChild { get; set; }
 
         public Node() {}
-        public Node(object value)
+
+        public Node(int value)
         {
             Value = value;
         }
