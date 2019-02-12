b8ec73b728c9c372d120abb936f0d7c0344bf826
CClemensJr <cclemensjr@gmail.com>
2019-02-11 17:44:43 -0800

    Added conditional to compare each character against other characters

--- Challenges/areCharactersUnique/AreCharactersUnique/Program.cs
+++ Challenges/areCharactersUnique/AreCharactersUnique/Program.cs
@@ -6,16 +6,26 @@ namespace AreCharactersUnique
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string word = "padre";
+            bool isUnique = AreCharactersUnique(word);
+
+            Console.WriteLine($"All characters in the word { word } are unique: { isUnique }");
+
+
+            Console.Write("Please press any key to exit...");
+            Console.ReadLine();
         }
 
         public static bool AreCharactersUnique(string toBeChecked)
         {
             for (int i = 0; i < toBeChecked.Length; i++)
             {
-                for (int j = 0; j < toBeChecked.Length; j++)
+                for (int j = 1; j < toBeChecked.Length; j++)
                 {
-
+                    if (toBeChecked[i] == toBeChecked[j])
+                    {
+                        return false;
+                    }
                 }
             }
 
