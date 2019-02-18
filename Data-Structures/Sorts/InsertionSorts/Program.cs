45f29eead6aad04f7fc109ec4c0d83a62dca5929
Charles Clemens <cclemensjr@gmail.com>
2019-02-18 09:19:17 -0800

    Added InsertionSort method

--- Data-Structures/Sorts/InsertionSorts/Program.cs
+++ Data-Structures/Sorts/InsertionSorts/Program.cs
@@ -8,5 +8,18 @@ namespace InsertionSorts
         {
             Console.WriteLine("Hello World!");
         }
+
+
+        /// <summary>
+        /// This method takes in an unsorted array and returns a sorted array using Insertion Sort
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns>A sorted array</returns>
+        public Array InsertionSort(Array array)
+        {
+            Array sortedArray = array;
+
+            return sortedArray;
+        }
     }
 }
