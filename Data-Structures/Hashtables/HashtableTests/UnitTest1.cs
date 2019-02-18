c915cfe4bf00324685079aec3886de4342bea12b
Charles Clemens <cclemensjr@gmail.com>
2019-02-18 08:00:34 -0800

    Added comments for each needed tests

--- Data-Structures/Hashtables/HashtableTests/UnitTest1.cs
+++ Data-Structures/Hashtables/HashtableTests/UnitTest1.cs
@@ -11,5 +11,15 @@ namespace HashtableTests
         {
 
         }
+
+        // Retrieving based on a key returns the value stored
+
+        // Successfully returns null for a key that does not exist in hashtable
+
+        // Successfully handles a collision within the hashtable
+
+        // Successfully retrieve a value from a bucket within the hashtable that has a collision
+
+        // Successfully hash a key to an in-range value
     }
 }
