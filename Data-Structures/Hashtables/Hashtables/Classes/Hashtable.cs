ed40809a54a7519425c3ab4c598498f7c97f31dc
Charles Clemens <cclemensjr@gmail.com>
2019-02-18 07:49:02 -0800

    Created method signature for add method.

--- Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
+++ Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace Hashtables.Classes
 {
-    class Hashtable
+    public class Hashtable
     {
+        /// <summary>
+        /// This method should hash the key then add the key and the value to the table, handling collisions as needed.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(string key, Object value)
+        {
+
+        }
+
+
     }
 }
