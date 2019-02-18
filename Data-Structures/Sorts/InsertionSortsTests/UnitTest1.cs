66ef5b6ecc6a6dad53e8e057afc7a9d2a475c4d3
Charles Clemens <cclemensjr@gmail.com>
2019-02-18 09:37:51 -0800

    Added test method comments and reference

--- Data-Structures/Sorts/InsertionSortsTests/UnitTest1.cs
+++ Data-Structures/Sorts/InsertionSortsTests/UnitTest1.cs
@@ -5,10 +5,19 @@ namespace InsertionSortsTests
 {
     public class UnitTest1
     {
+        // A randomly generated unsorted array returns the array sorted
         [Fact]
         public void Test1()
         {
 
         }
+
+        // A sorted array returns the same sorted array
+
+        // A backwards-sorted array returns the array sorted
+
+        // An empty array returns the same empty array
+
+        // An array of one element returns the same single-element array
     }
 }
