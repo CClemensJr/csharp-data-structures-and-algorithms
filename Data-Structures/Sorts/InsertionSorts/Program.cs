0526aaa2c1f0c11cc70a4a4b7b733435d88056bb
CClemensJr <cclemensjr@gmail.com>
2019-02-23 13:22:41 -0800

    Created random method to populate array

--- Data-Structures/Sorts/InsertionSorts/Program.cs
+++ Data-Structures/Sorts/InsertionSorts/Program.cs
@@ -2,11 +2,23 @@
 
 namespace InsertionSorts
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int min = 1;
+            int max = 100;
+            int size = 10;
+            int[] unsortedArray = new int[size];
+            
+            for (int i = 0; i < unsortedArray.Length; i++)
+            {
+                unsortedArray[i] = RandomNumber(min, max);
+            }
+
+
+            Console.Write("Press any key to continue...");
+            Console.ReadLine();
         }
 
 
@@ -15,11 +27,25 @@ namespace InsertionSorts
         /// </summary>
         /// <param name="array"></param>
         /// <returns>A sorted array</returns>
-        public Array InsertionSort(Array array)
+        public static Array InsertionSort(Array array)
         {
             Array sortedArray = array;
 
             return sortedArray;
+        }
+
+
+        /// <summary>
+        /// This method takes in a minimum value and a maximum value then returns a random number between the two.
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns>A random number between the min and max values</returns>
+        public static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+
+            return random.Next(min, max);
         }
     }
 }
