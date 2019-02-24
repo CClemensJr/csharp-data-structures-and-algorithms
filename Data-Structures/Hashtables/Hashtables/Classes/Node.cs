53d87b1d5d2c41d66851630598de4b7ce05482ea
CClemensJr <cclemensjr@gmail.com>
2019-02-23 23:33:55 -0800

    added constructor

--- Data-Structures/Hashtables/Hashtables/Classes/Node.cs
+++ Data-Structures/Hashtables/Hashtables/Classes/Node.cs
@@ -8,5 +8,16 @@ namespace Hashtables.Classes
     {
         public string Key { get; set; }
         public object Value { get; set; }
+
+        /// <summary>
+        /// This custome constructor allows key and value to be set upon instantiation
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public Node(string key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
     }
 }
