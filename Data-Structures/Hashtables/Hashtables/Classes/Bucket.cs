7fcd8db9fe73a13bbd8b631386c661c8d8faa023
CClemensJr <cclemensjr@gmail.com>
2019-02-24 09:51:48 -0800

    Changed node to bucket

--- Data-Structures/Hashtables/Hashtables/Classes/Bucket.cs
+++ Data-Structures/Hashtables/Hashtables/Classes/Bucket.cs
@@ -4,24 +4,24 @@ using System.Text;
 
 namespace Hashtables.Classes
 {
-    public class Node
+    public class Bucket
     {
         public string Key { get; set; }
         public object Value { get; set; }
-        public Node Next { get; set; }
+        public Bucket Next { get; set; }
 
-        public Node() { }
+        public Bucket () { }
 
         /// <summary>
         /// This custome constructor allows key and value to be set upon instantiation
         /// </summary>
         /// <param name="key"></param>
         /// <param name="value"></param>
-        public Node(string key, object value)
+        public Bucket(string key, object value)
         {
             Key = key;
             Value = value;
-            Next = new Node();
+            Next = new Bucket();
         }
     }
 }
