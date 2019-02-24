0a44331058eba74278ac55aa4568a4a2ec529b17
CClemensJr <cclemensjr@gmail.com>
2019-02-23 16:56:15 -0800

    Added custom constructor

--- Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
+++ Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
@@ -8,7 +8,14 @@ namespace Hashtables.Classes
     public class Hashtable
     {
         public int Size { get; set; }
-        public List<object> table { get; set; }
+        public object[] Table { get; set; }
+
+
+        public Hashtable(int size)
+        {
+            Size = size;
+            Table = new object[Size];
+        }
 
         /// <summary>
         /// This method hashes the key then adds the key and the value to the table, handling collisions as needed.
@@ -18,11 +25,9 @@ namespace Hashtables.Classes
         public void Add(string key, Object value)
         {
             int index = Hash(key);
-            Hashtable hashtable = new Hashtable();
+            Hashtable hashtable = new Hashtable(index + 1);
 
-            hashtable.table[index] = value;
-      
-            Size++;
+            Table[index] = value;
         }
 
 
