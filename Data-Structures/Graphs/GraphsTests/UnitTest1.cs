4a7df1f3ed6e47ad7dc4edb965d2e42e5c52f11a
Charles Clemens <cclemensjr@gmail.com>
2019-02-25 11:47:26 -0800

    Added test method signatures

--- Data-Structures/Graphs/GraphsTests/UnitTest1.cs
+++ Data-Structures/Graphs/GraphsTests/UnitTest1.cs
@@ -5,8 +5,58 @@ namespace GraphsTests
 {
     public class UnitTest1
     {
+        // Node can be successfully added to the graph
         [Fact]
-        public void Test1()
+        public void CanAddNodeToGraph()
+        {
+
+        }
+
+        // An edge can be successfully added to the graph
+        [Fact]
+        public void CanAddEdgeToGraph()
+        {
+
+        }
+
+        // A collection of all nodes can be properly retrieved from the graph
+        [Fact]
+        public void CanGetAllNodesInGraph()
+        {
+
+        }
+
+        // All appropriate neighbors can be retrieved from the graph
+        [Fact]
+        public void CanGetNeighborsInGraph()
+        {
+
+        }
+
+        // Neighbors are returned with the weight between nodes included
+        [Fact]
+        public void CanGetWeightBetweenNodes()
+        {
+
+        }
+
+        // The proper size is returned, representing the number of nodes in the graph
+        [Fact]
+        public void CanGetSizeOfGraph()
+        {
+
+        }
+
+        // A graph with only one node and edge can be properly returned
+        [Fact]
+        public void CanReturnSingleNodeGraph()
+        {
+
+        }
+
+        // An empty graph properly returns null
+        [Fact]
+        public void CanGetEmptyGraph()
         {
 
         }
