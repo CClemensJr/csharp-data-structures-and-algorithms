c9941a066816172baeca90bb5159185d8b03c8fa
Charles Clemens <cclemensjr@gmail.com>
2019-02-25 11:53:12 -0800

    Added method signatures

--- Data-Structures/Graphs/Graphs/Classes/Graph.cs
+++ Data-Structures/Graphs/Graphs/Classes/Graph.cs
@@ -4,7 +4,43 @@ using System.Text;
 
 namespace Graphs.Classes
 {
-    class Graph
+    public class Graph
     {
+        // Adds a new node to the graph
+        // Takes in the value of that node
+        // Returns the added node
+        public void AddNode()
+        {
+
+        }
+
+        // Adds a new edge between two nodes in the graph
+        // Include the ability to have a “weight”
+        // Takes in the two nodes to be connected by the edge
+        // Both nodes should already be in the Graph
+        public void AddEdge()
+        {
+
+        }
+
+        // Returns all of the nodes in the graph as a collection(set, list, or similar)
+        public void GetNodes()
+        {
+
+        }
+
+        // Returns a collection of nodes connected to the given node
+        // Takes in a given node
+        // Include the weight of the connection in the returned collection
+        public void GetNeighbors()
+        {
+
+        }
+
+        // Returns the total number of nodes in the graph
+        public void Size()
+        {
+
+        }
     }
 }
